f837954a92b41ab3cdf647b3f23d00ccdfedf27f
Max Manayan <maximillian521@gmail.com>
2021-08-26 09:10:56 -0600

    sic skeleton server setup and functional

--- w0-node-express/socket-io-chat/index.js
+++ w0-node-express/socket-io-chat/index.js
@@ -3,4 +3,28 @@
 // Assignment: mthree, socket.io chat
 // Date: August 26, 2021
 
+// links
+// Socket.io chat docs - [ https://socket.io/get-started/chat ]
+
 // imports
+const express = require("express");
+const app = express();
+const http = require("http"); // what is this for?
+const server = http.createServer(app);
+
+// constants
+const PORT = 3000;
+
+// APIs and middleware
+
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Socket.io Chat App</h1>");
+});
+
+// error handlers
+
+// server startup
+server.listen(PORT, () => {
+  console.log(`Socket.io Chat listening on http://localhost:${PORT}`);
+});
