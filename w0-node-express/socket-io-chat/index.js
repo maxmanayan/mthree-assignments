89a6fa961a0caa31a7ec1a3e0e2b7f0cc3522035
Max Manayan <maximillian521@gmail.com>
2021-08-26 09:35:09 -0600

    sic socket instance created and socket listener added

--- w0-node-express/socket-io-chat/index.js
+++ w0-node-express/socket-io-chat/index.js
@@ -9,9 +9,11 @@
 // imports
 const express = require("express");
 const app = express();
-const http = require("http"); // what is this for?
-const server = http.createServer(app);
+const http = require("http");
+const server = http.createServer(app); // creates http server
 const morgan = require("morgan");
+const { Server } = require("socket.io");
+const io = new Server(server); // initializes new instance of socket.io by passing the HTTP server object
 
 // constants
 const PORT = 3000;
@@ -21,12 +23,16 @@ app.use(morgan("dev"));
 
 // routes
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.status(200).sendFile(__dirname + "/index.html");
 });
 
 // error handlers
 
 // server startup
+io.on("connection", (socket) => {
+  console.log("a user connected");
+}); // listens on the 'connection' event for incoming sockets
+
 server.listen(PORT, () => {
   console.log(`Socket.io Chat listening on http://localhost:${PORT}`);
 });
