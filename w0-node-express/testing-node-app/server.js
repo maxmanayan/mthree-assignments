46db88865459c77150f6efa6bd8b65fdb184c64a
Max Manayan <maximillian521@gmail.com>
2021-08-26 11:33:26 -0600

    tna - uuid package added and setup in sample data

--- w0-node-express/testing-node-app/server.js
+++ w0-node-express/testing-node-app/server.js
@@ -9,47 +9,50 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const { v4: uuidv4 } = require("uuid");
 
 // constants
 const PORT = 3000;
 
 const nbaPlayers = [
   {
-    _id: "uuid",
+    _id: uuidv4(),
     testId: 1,
     name: "Lebron James",
     team: "Lakers",
     jerseyNum: 6,
   },
   {
-    _id: "uuid",
+    _id: uuidv4(),
     testId: 2,
     name: "Kevin Durant",
     team: "Nets",
     jerseyNum: 7,
   },
   {
-    _id: "uuid",
+    _id: uuidv4(),
     testId: 3,
     name: "Giannis Antetokounmpo",
     team: "Bucks",
     jerseyNum: 34,
   },
   {
-    _id: "uuid",
+    _id: uuidv4(),
     testId: 4,
     name: "Russell Westbrook",
     team: "Lakers",
     jerseyNum: 0,
   },
   {
-    _id: "uuid",
+    _id: uuidv4(),
     testId: 5,
     name: "Damian Lillard",
     team: "Trail Blazers",
     jerseyNum: 0,
   },
 ];
+
+console.log(nbaPlayers);
 
 // APIs and middleware
 app.use(morgan("dev"));
