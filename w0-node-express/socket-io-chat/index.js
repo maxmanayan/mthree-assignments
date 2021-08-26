589b25d349a5d772ce5d67f2fda00c317083e3a6
Max Manayan <maximillian521@gmail.com>
2021-08-26 09:29:43 -0600

    morgan middleware added to server

--- w0-node-express/socket-io-chat/index.js
+++ w0-node-express/socket-io-chat/index.js
@@ -11,11 +11,13 @@ const express = require("express");
 const app = express();
 const http = require("http"); // what is this for?
 const server = http.createServer(app);
+const morgan = require("morgan");
 
 // constants
 const PORT = 3000;
 
 // APIs and middleware
+app.use(morgan("dev"));
 
 // routes
 app.get("/", (req, res) => {
