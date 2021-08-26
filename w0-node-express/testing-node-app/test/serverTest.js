d06b6932ea385a2448afb500aa6aa36ddfdcaf39
Max Manayan <maximillian521@gmail.com>
2021-08-26 13:47:08 -0600

    tna - chai-http package installed and chai and mocha moved to dev dependencies

--- w0-node-express/testing-node-app/test/serverTest.js
+++ w0-node-express/testing-node-app/test/serverTest.js
@@ -5,15 +5,65 @@
 
 // links
 // Mocha (test runner) docs = [ https://mochajs.org/#getting-started ]
-// by default, Mocha will look for a folder called 'test'
 // Chai (assertion library) docs = [ https://www.chaijs.com/ ]
+// Setup Testing YT Tutorial - [ https://www.youtube.com/watch?v=MLTRHc5dk6s ]
+// Testing REST Api Blog - [ https://dev.to/mhmdlotfy96/testing-a-rest-api-in-node-js-with-express-using-mocha-and-chai-1258 ]
 
+// By default, Mocha will look for a folder called 'test'
 // imports
-const assert = require("chai").assert;
-const { sayHello } = require("../server");
+const chai = require("chai");
+const server = require("../server");
+const { v4: uuidv4 } = require("uuid");
 
-describe("Server", function () {
-  it("function should say hello", function () {
-    assert.equal(sayHello(), "Hello");
+const nbaPlayers = [
+  {
+    _id: uuidv4(),
+    testId: 1,
+    name: "Lebron James",
+    team: "Lakers",
+    jerseyNum: 23,
+  },
+  {
+    _id: uuidv4(),
+    testId: 2,
+    name: "Kevin Durant",
+    team: "Nets",
+    jerseyNum: 7,
+  },
+  {
+    _id: uuidv4(),
+    testId: 3,
+    name: "Giannis Antetokounmpo",
+    team: "Bucks",
+    jerseyNum: 34,
+  },
+  {
+    _id: uuidv4(),
+    testId: 4,
+    name: "Russell Westbrook",
+    team: "Lakers",
+    jerseyNum: 0,
+  },
+  {
+    _id: uuidv4(),
+    testId: 5,
+    name: "Damian Lillard",
+    team: "Trail Blazers",
+    jerseyNum: 0,
+  },
+];
+
+// REST API tests
+describe("REST APIs", () => {
+  describe("GET endpoints", () => {
+    it("It should get whole sample data", (done) => {
+      chai
+        .request(server)
+        .get("/players")
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
   });
 });
