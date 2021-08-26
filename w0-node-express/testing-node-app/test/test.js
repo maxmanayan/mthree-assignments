7315f8367511014c83d091a767bde6c9c377ab68
Max Manayan <maximillian521@gmail.com>
2021-08-26 13:02:34 -0600

    tna - test file setup

---
+++ w0-node-express/testing-node-app/test/test.js
@@ -0,0 +1,20 @@
+// File: /test/test.js
+// Created by: Max Manayan
+// Assignment: mthree, Testing Node.js Application
+// Date: August 26, 2021
+
+// links
+// Mocha docs = [ https://mochajs.org/#getting-started ]
+// Chai docs = [ https://www.chaijs.com/ ]
+
+// imports
+const chai = require("chai");
+const assert = chai.assert;
+const mocha = require("mocha");
+const describe = mocha.describe;
+const it = mocha.it;
+
+// console.log(assert);
+// console.log(mocha);
+console.log(describe);
+console.log(it);
